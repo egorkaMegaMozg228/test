b53f1ca4d00fa10f76918645dd355e9ec58e1730
egorkaMegaMozg228 <zemtsov.yehor@gmail.com>
2024-09-17 21:51:42 +0300

    Add files via upload

---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a, z1, z2; //вхідний параметр і результати обчислень
+	cout << "a = "; cin >> a;
+	z1 = 1 - (sin(2 * a) * sin(2 * a) / 4) + cos(2 * a);
+	z2 = cos(a) * cos(a) + cos(a) * cos(a) * cos(a) * cos(a);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
